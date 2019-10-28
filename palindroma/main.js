750fd211e5484ad8602f4540953c65542e7bc438
dev-prime99 <prime99@icloud.com>
2019-10-28 18:45:58 +0100

    completed exs

--- palindroma/main.js
+++ palindroma/main.js
@@ -3,6 +3,7 @@ var parola, contrario;
 
 parola = prompt("Inserisci una parola").toLowerCase();
 
+/*
 contrario = parola.split('').reverse().join("");
 //console.log(contrario);
 
@@ -10,4 +11,20 @@ if (parola == contrario){
   console.log(true);
 } else{
   console.log(false);
+}
+*/
+
+contrario = '';
+
+for(var i = parola.length-1; i >= 0; i--){
+    contrario += parola[i];
+}
+console.log(contrario);
+
+if (parola == contrario){
+  console.log(true);
+  document.getElementById('risultato').innerHTML = "La parola " + parola + " è polindroma " + "(" + contrario + ")";
+} else{
+  console.log(false);
+  document.getElementById('risultato').innerHTML = "La parola " + parola + " non è polindroma " + "(" + contrario + ")";
 }
--- paridispari/main.js
+++ paridispari/main.js
@@ -20,6 +20,7 @@ while (ok < 1){
   nu = parseInt(prompt("Inserisci un numero da 1 a 5"));
   if (nu > 0 && nu < 6){
     ok = 1;
+    document.getElementById('user').innerHTML = nu;
   } else{
     ok = 0;
     alert("Inserisci un numero compreso tra 1 e 5");
@@ -28,13 +29,17 @@ while (ok < 1){
 
 npc = Math.floor(Math.random() * 5) + 1;
 console.log(npc);
+document.getElementById('cpu').innerHTML = npc;
 
 somma = nu + npc;
 
 if (somma % 2 == 0 && pod == "pari"){
   console.log("vinto");
+  document.getElementById('risultato').innerHTML = "Hai vinto";
 } else if (somma%2==1 && pod == "dispari"){
+  document.getElementById('risultato').innerHTML = "Hai vinto";
   console.log("vinto1");
 } else{
+  document.getElementById('risultato').innerHTML = "Hai perso";
   console.log("perso");
 }
