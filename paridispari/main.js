6d9e4a20802f8835dc5366872b6b75c6d7c2d6e7
dev-prime99 <prime99@icloud.com>
2019-10-28 17:52:44 +0100

    fixed user error js

--- paridispari/main.js
+++ paridispari/main.js
@@ -1,20 +1,37 @@
 
-var pod, nu, npc, somma;
+var pod, nu, npc, somma, ok;
 
-pod = prompt("Pari o dispari").toLowerCase();
+ok = 0;
 
-nu = parseInt(prompt("Inserisci un numero da 1 a 5"));
+while (ok < 1){
+  pod = prompt("Pari o dispari").toLowerCase();
+  if (pod != "pari" && pod != "dispari"){
+    ok = 0;
+    alert("Inserisci pari o dispari, non altre parole");
+  } else{
+    ok = 1;
+  }
+  console.log(pod);
+}
+
+ok = 0;
+
+while (ok < 1){
+  nu = parseInt(prompt("Inserisci un numero da 1 a 5"));
+  if (nu > 0 && nu < 6){
+    ok = 1;
+  } else{
+    ok = 0;
+    alert("Inserisci un numero compreso tra 1 e 5");
+  }
+}
 
 npc = Math.floor(Math.random() * 5) + 1;
 console.log(npc);
 
 somma = nu + npc;
 
-if (nu > 5){
-  location.reload();
-} else if(pod != "pari" || pod != "dispari"){
-  location.reload();
-} else if (somma%2==0 && pod == "pari"){
+if (somma % 2 == 0 && pod == "pari"){
   console.log("vinto");
 } else if (somma%2==1 && pod == "dispari"){
   console.log("vinto1");
